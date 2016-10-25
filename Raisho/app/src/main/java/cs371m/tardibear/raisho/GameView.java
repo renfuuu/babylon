be037c23749c206e4394ee4da80bb27af92fb91b
Itoro <esiere.i@gmail.com>
2016-10-25 13:35:54 -0500

    Start of Custom View

---
+++ Raisho/app/src/main/java/cs371m/tardibear/raisho/GameView.java
@@ -0,0 +1,15 @@
+package cs371m.tardibear.raisho;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by itoro on 10/25/16.
+ */
+
+public class GameView extends View {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
