43cfc1a2b2ecb77aac2410024f037e984eac3a7f
Itoro <esiere.i@gmail.com>
2016-10-25 13:21:57 -0500

    play button

--- Raisho/app/src/main/java/cs371m/tardibear/raisho/CoverLetter.java
+++ Raisho/app/src/main/java/cs371m/tardibear/raisho/CoverLetter.java
@@ -1,5 +1,6 @@
 package cs371m.tardibear.raisho;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class CoverLetter extends AppCompatActivity {
 
@@ -15,17 +17,16 @@ public class CoverLetter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button playButton = (Button) findViewById(R.id.button_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent mainGame = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainGame);
             }
-        });
+    });
+
     }
 
     @Override
